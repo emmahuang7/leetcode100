a3646f8baf97dff1144cf3c5522917733e31c187
emmahuang7 <62838008+emmahuang7@users.noreply.github.com>
2020-06-03 21:17:10 +0200

    Update code.py

--- 21.合并两个有序链表/code.py
+++ 21.合并两个有序链表/code.py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 and l2:
+            if l1.val>=l2.val:
+                cur.next = l2
+                l2 = l2.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
