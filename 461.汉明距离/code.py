8a0295b02eb445c14ff7fb7f41313c71b466ecb3
emmahuang7 <62838008+emmahuang7@users.noreply.github.com>
2020-06-03 19:12:52 +0200

    Update code.py

--- 461.汉明距离/code.py
+++ 461.汉明距离/code.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        tmp = x^y
+        d = 0
+        while tmp:
+            tmp = tmp&(tmp-1)
+            d += 1
+        return d
