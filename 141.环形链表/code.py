bbc70924585db14a88febf0e982cfca9325985cd
emmahuang7 <62838008+emmahuang7@users.noreply.github.com>
2020-06-09 00:03:40 +0200

    Update code.py

--- 141.环形链表/code.py
+++ 141.环形链表/code.py
@@ -1,3 +1,4 @@
+#思路1：快慢指针
 class Solution(object):
     def hasCycle(self, head):
         """
@@ -13,3 +14,19 @@ class Solution(object):
             slow = slow.next
             fast = fast.next.next
         return True 
+#思路2：哈希表
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return False
+        d = {}
+        while head:
+            if head in d:
+                return True
+            d[head] = 1
+            head = head.next
+        return False
