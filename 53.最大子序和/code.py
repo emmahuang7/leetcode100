9677b0214695bf086994b71eafbe66d6c9576264
emmahuang7 <62838008+emmahuang7@users.noreply.github.com>
2020-06-08 23:32:15 +0200

    Create code.py

---
+++ 53.最大子序和/code.py
@@ -0,0 +1,36 @@
+#错误！！
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for idx,num in enumerate(nums):
+            d[num] = idx
+         # 不能遍历元素，只能遍历坐标
+         #元素可能重复，没有办法处理
+         #[3,3]和target=6的情况
+        for num in nums:
+            idx_a = d.get(target-num)
+            if idx_a and idx_a != d[num]:
+                return d[num],idx_a
+        return None 
+
+# 正确      
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for idx,num in enumerate(nums):
+            d[num] = idx
+        for idx in range(len(nums)):
+            idx_a = d.get(target-nums[idx])
+            if idx_a and idx_a != idx:
+                return idx,idx_a
+        return None 
